8c3ee008435fd0249a9b58be06d56d00aa8013fb
Ethan Orcutt <izzaboy306@gmail.com>
2016-11-23 23:12:26 -0600

    Fixed a bad route in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 	root to: 'pages#home'
-	devise_for :users
 	devise_for :users, controllers: { registrations: 'users/registrations' }
 	get 'about', to: 'pages#about'
 	resources :contacts, only: :create
